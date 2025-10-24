2c48dfd90130a46c4b89101a54d0f92fab8b4de6
aotife <92732685+aotife@users.noreply.github.com>
2025-10-25 00:29:36 +0800

    Update Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,7 @@
 import { ReactNode } from "react";
-import { Box, Flex, Text, Container, Theme } from "@radix-ui/themes";
-import { Separator, Button, Toaster } from "./ui";
+import { Box, Flex, Theme } from "@radix-ui/themes";
+import { Toaster } from "./ui";
 import Navbar from "./Navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRss } from "@fortawesome/free-solid-svg-icons";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
 
 interface LayoutProps {
@@ -13,77 +9,21 @@ interface LayoutProps {
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  const currentYear = new Date().getFullYear();
+  // 保留useTranslation是为了避免未使用变量报错，也可以直接删除
   const { t } = useTranslation();
 
   return (
     <Theme appearance="light">
-      <Flex direction="column" className="min-h-[100vh]" >
+      <Flex direction="column" className="min-h-[100vh]">
         {/* 顶部导航栏 */}
         <Navbar />
 
         {/* 主要内容 */}
         <Box className="grow px-2">{children}</Box>
 
-        {/* 页脚 */}
-        <Box>
-          <Container>
-            <Separator color="gray" />
-            <Flex justify="center" align="center" py="3" direction="column">
-              <Text size="2" color="gray">
-                {t("footer.copyright", { year: currentYear })}
-              </Text>
-              <Flex gap="3" mt="2" direction={{ initial: "column", sm: "row" }}>
-                <Button variant="link" asChild>
-                  <a
-                    href="https://zaunist.com"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  
-                  >
-                    <FontAwesomeIcon
-                      icon={faRss}
-                      size="lg"
-                     
-                    />
-                    <Text size="2">{t("footer.blog")}</Text>
-                  </a>
-                </Button>
-                <Button variant="link" asChild>
-                  <a
-                    href="https://www.youtube.com/@zaunist"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                
-                  >
-                    <FontAwesomeIcon
-                      icon={faYoutube}
-                      size="lg"
-                  
-                    />
-                    <Text size="2">{t("footer.youtube")}</Text>
-                  </a>
-                </Button>
-                <Button variant="link" asChild>
-                  <a
-                    href="https://mail.mdzz.uk"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  
-                  >
-                    <FontAwesomeIcon
-                      icon={faEnvelope}
-                      size="lg"
-                   
-                    />
-                    <Text size="2">{t("footer.tempMail")}</Text>
-                  </a>
-                </Button>
-              </Flex>
-            </Flex>
-          </Container>
-        </Box>
-        <Toaster /> 
+        {/* 页脚已删除 */}
+        
+        <Toaster />
       </Flex>
     </Theme>
   );
